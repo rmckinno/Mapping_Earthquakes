4fc3b28c2ae7490690fbecec7f9f907d4809e8f5
Rachel McKinnon <rachel.e.mckinnon@gmail.com>
2021-11-11 00:52:30 -0600

    Add toggle dark/street to map.

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,6 +1,3 @@
-// Create the map object with a center and zoom level.
-let map = L.map("mapid").setView([30, 30], 2);
-
 // Create tile layer for background of map.
 let streets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
@@ -11,8 +8,32 @@ let streets = L.tileLayer(
     accessToken: API_KEY,
   }
 );
-// Add 'streets' tile layer to the map.
-streets.addTo(map);
+
+// create dark tile layer
+let dark = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+// create base layer that holds both maps
+let baseMaps = {
+  Street: streets,
+  Dark: dark
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+  center: [30, 30],
+  zoom: 2,
+  layers: [streets]
+})
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing airport GeoJSON url
 let airportData =
