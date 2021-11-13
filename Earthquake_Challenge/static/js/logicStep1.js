0237830292bc5133a96b614ea6540d9d344f67e2
Rachel McKinnon <rachel.e.mckinnon@gmail.com>
2021-11-12 21:32:20 -0600

    Add new Earthquake folder and subfolders.

---
+++ Earthquake_Challenge/static/js/logicStep1.js
@@ -0,0 +1,47 @@
+// Create tile layer for background of map.
+let streets = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
+// create satellite street tile layer
+let satelliteStreets = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+// create base layer that holds both maps
+let baseMaps = {
+  Streets: streets,
+  "Satellite Street": satelliteStreets,
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map("mapid", {
+  center: [39.5, -98.5],
+  zoom: 3,
+  layers: [streets],
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing latest earthquake GeoJSON url
+let earthquakes =
+  "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+// Grab data from URL
+d3.json(earthquakes).then((data) => {
+  console.log(data);
+  
+  // create geojson layer
+  L.geoJSON(data).addTo(map);
+});
