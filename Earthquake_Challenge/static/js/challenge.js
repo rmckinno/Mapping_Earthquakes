32e225f4ac6e2cbaf9a2a80863edc22b4a26ee00
Rachel McKinnon <rachel.e.mckinnon@gmail.com>
2021-11-13 03:12:55 -0600

    Add third tile layer to map.

--- Earthquake_Challenge/static/js/challenge.js
+++ Earthquake_Challenge/static/js/challenge.js
@@ -1,7 +1,5 @@
-// Add console.log to check to see if our code is working.
-console.log("working");
 
-// We create the tile layer that will be the background of our map.
+// Create the tile layer that will be the background of our map.
 let streets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
@@ -12,9 +10,21 @@ let streets = L.tileLayer(
   }
 );
 
-// We create the second tile layer that will be the background of our map.
+// Create the second tile layer that will be the background of our map.
 let satelliteStreets = L.tileLayer(
   "https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}",
+  {
+    attribution:
+      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY,
+  }
+);
+
+// Create third tile layer for background of map.
+// Create the second tile layer that will be the background of our map.
+let dark = L.tileLayer(
+  "https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}",
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -34,6 +44,7 @@ let map = L.map("mapid", {
 let baseMaps = {
   Streets: streets,
   Satellite: satelliteStreets,
+  Dark: dark
 };
 
 // Add a 3rd layer group for major earthquake data.
